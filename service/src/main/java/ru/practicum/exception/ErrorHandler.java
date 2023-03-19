750bfabccc8c9710242e1002f33544f9f149a546
Mikhail Gorbunov <mixalich84@yandex.ru>
2023-03-19 18:22:58 +0300

    feat: Изменены ErrorHandler,ErrorResponse

--- service/src/main/java/ru/practicum/exception/ErrorHandler.java
+++ service/src/main/java/ru/practicum/exception/ErrorHandler.java
@@ -5,13 +5,17 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 @RestControllerAdvice
 public class ErrorHandler {
+    public static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
     @ExceptionHandler
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public ErrorResponse categoryNotFoundExceptionResponse(final CategoryNotFoundException e) {
-        return new ErrorResponse(e.getMessage()
-        );
+        return new ErrorResponse(e.getMessage(),
+                "NOT_FOUND", "The required object was not found.", LocalDateTime.now().format(FORMATTER));
     }
 
 }
--- service/src/main/java/ru/practicum/exception/ErrorResponse.java
+++ service/src/main/java/ru/practicum/exception/ErrorResponse.java
@@ -1,14 +1,21 @@
 package ru.practicum.exception;
 
+import lombok.Getter;
+
+@Getter
 public class ErrorResponse {
 
-    private final String error;
 
-    public ErrorResponse(String error) {
-        this.error = error;
+    private final String status;
+    private final String reason;
+    private final String message;
+    private final String timestamp;
+
+    public ErrorResponse(String message, String status, String reason, String timestamp) {
+        this.message = message;
+        this.status = status;
+        this.reason = reason;
+        this.timestamp = timestamp;
     }
 
-    public String getError() {
-        return error;
-    }
 }
